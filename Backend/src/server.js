de7e3eb85b7b71fa013b904ef61e7b2ed6ff3fe5
AnderThomaz <150291677+AnderThomaz@users.noreply.github.com>
2024-02-01 15:10:08 -0300

    att

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,12 +5,12 @@ dotenv.config()
 
 const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 
-wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
-  
-    ws.on('message', function message(data) {
-      wss.client.forEach((clients)=> clients.send(data.toString()))
-    });
-  
-    console.log("Client Connector");
-  });
+wss.on("connection", (ws) => {
+    ws.on("error", console.error)
+
+    ws.on("message", (data) => {
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+
+    console.log("client connected")
+})
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,12 +1,19 @@
 // login elementos
 
 const login = document.querySelector(".login")
-const loginForm = login.querySelector(".login_forms")
-const loginInput = login.querySelector(".login_inpult")
+const loginForm = document.querySelector(".login_forms")
+const loginInput = document.querySelector(".login_inpult")
 
+const user = {id: "", name:"", color:""}
 
 const handleSubmit = (event) => {
     event.preventDefault()
+
+    user.id = crypto.randomUUID()
+
+    user.name = loginInput.value
+
+    console.log(user)
 }
 
 loginForm.addEventListener("submit", handleSubmit)
